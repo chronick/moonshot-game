8f0fda8c3e383253dc7df9ce6bb66cf8d1dce256
Nick Donohue <ndonohue@gmail.com>
2020-11-08 15:00:14 -0500

    Add Tests

---
+++ Assets/Tests/Utils/InGameDateTime.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Utils {
+    public class InGameDateTimeConversions {
+        [Test]
+        public void DateTimeSecond() {
+            Assert.AreEqual(1, global::Utils.InGameDateTimeConversions.Second(1));
+            Assert.AreEqual(0, global::Utils.InGameDateTimeConversions.Second(60));
+        }
+
+        [Test]
+        public void DateTimeMinute() {
+            Assert.AreEqual(0, global::Utils.InGameDateTimeConversions.Minute(1));
+            Assert.AreEqual(1, global::Utils.InGameDateTimeConversions.Minute(60));
+            Assert.AreEqual(0, global::Utils.InGameDateTimeConversions.Minute(3600));
+        }
+
+        [Test]
+        public void DateTimeHour() {
+            Assert.AreEqual(0, global::Utils.InGameDateTimeConversions.Hour(1));
+            Assert.AreEqual(1, global::Utils.InGameDateTimeConversions.Hour(3600));
+            Assert.AreEqual(0, global::Utils.InGameDateTimeConversions.Hour(86400));
+        }
+    }
+}
